e2085e1a31ec01715eceac8c72b93911e129e89a
marek.poczatek <marek.poczatek@icloud.com>
2018-06-29 16:25:12 +0200

    Added SQL scripts.

---
+++ DbScripts/001 - CREATE - Contributions.sql
@@ -0,0 +1,29 @@
+-- USE [ZusContribution]
+-- GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Contributions](
+	[ContributionId] [int] IDENTITY(1,1) NOT NULL,
+	[ValidityBeginDate] [datetime] NOT NULL,
+	[ValidityEndDate] [datetime] NOT NULL,
+	[SocialInsuranceBasisOfContributionDimension] [money] NOT NULL,
+	[PensionInsuranceCost] [money] NOT NULL,
+	[DisabilityInsuranceCost] [money] NOT NULL,
+	[MedicalInsuranceCost] [money] NOT NULL,
+	[AccidentInsuranceCost] [money] NOT NULL,
+	[LaborFundBasisCost] [money] NOT NULL,
+	[HealthInsuranceBasisOfContributionDimension] [money] NOT NULL,
+	[HealthInsuranceCost] [money] NOT NULL,
+	[HealthInsuranceTaxDeductible] [money] NOT NULL,
+	[TotalContribution] [money] NOT NULL,
+ CONSTRAINT [PK_Contribution] PRIMARY KEY CLUSTERED 
+(
+	[ContributionId] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
---
+++ DbScripts/002 - CREATE - SynchronizationLogs.sql
@@ -0,0 +1,20 @@
+-- USE [ZusContribution]
+-- GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[SynchronizationLogs](
+	[SynchronizationLogId] [int] IDENTITY(1,1) NOT NULL,
+	[ValidityEndDate] [datetime] NOT NULL,
+	[SynchronizationDate] [datetime] NOT NULL,
+	[IsSuccess] [bit] NOT NULL
+ CONSTRAINT [PK_SynchronizationLog] PRIMARY KEY CLUSTERED 
+(
+	[SynchronizationLogId] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
